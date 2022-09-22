d9deb2bfbb1d884e8768c428f1073ecb07926765
Bhaskar Thakur <bhaskar.dezire@gmail.com>
2022-09-22 10:48:58 +0530

    Fix prior tests

--- tests/unit/tornado_app_handlers_test.py
+++ tests/unit/tornado_app_handlers_test.py
@@ -1,13 +1,15 @@
-# Copyright (c) 2020. All rights reserved.
-
+import aiotask_context as context  # type: ignore
 import atexit
 from io import StringIO
 import json
+import logging
+import logging.config
 import yaml
 
 from tornado.ioloop import IOLoop
 import tornado.testing
 
+from notesservice import LOGGER_NAME
 from notesservice.tornado.app import make_notesservice_app
 
 from data import notes_data_suite
@@ -16,6 +18,14 @@ from data import notes_data_suite
 IN_MEMORY_CFG_TXT = '''
 service:
   name: Notes
+
+notes-db:
+  memory: null
+
+logging:
+  version: 1
+  root:
+    level: ERROR
 '''
 
 with StringIO(IN_MEMORY_CFG_TXT) as f:
@@ -33,9 +43,13 @@ class NotesServiceTornadoAppTestSetup(tornado.testing.AsyncHTTPTestCase):
         self.addr1 = notes_data[keys[1]]
 
     def get_app(self) -> tornado.web.Application:
+        logging.config.dictConfig(TEST_CONFIG['logging'])
+        logger = logging.getLogger(LOGGER_NAME)
+
         notes_service, app = make_notesservice_app(
             config=TEST_CONFIG,
-            debug=True
+            debug=True,
+            logger=logger
         )
 
         notes_service.start()
@@ -44,7 +58,9 @@ class NotesServiceTornadoAppTestSetup(tornado.testing.AsyncHTTPTestCase):
         return app
 
     def get_new_ioloop(self):
-        return IOLoop.current()
+        instance = IOLoop.current()
+        instance.asyncio_loop.set_task_factory(context.task_factory)
+        return instance
 
 
 class NotesServiceTornadoAppUnitTests(NotesServiceTornadoAppTestSetup):
